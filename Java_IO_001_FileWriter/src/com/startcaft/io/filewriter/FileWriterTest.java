391f911bfd6d2f8c26dd77cc57fb5904317709aa
51418996 <51418996@qq.com>
2015-06-15 15:21:06 +0800

    标准的IO异常处理

--- Java_IO_001_FileWriter/src/com/startcaft/io/filewriter/FileWriterTest.java
+++ Java_IO_001_FileWriter/src/com/startcaft/io/filewriter/FileWriterTest.java
@@ -2,8 +2,10 @@ package com.startcaft.io.filewriter;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.chrono.JapaneseChronology;
 
 import org.junit.Test;
 
@@ -21,7 +23,7 @@ public class FileWriterTest {
 	@Test
 	public void writerContentToFile() throws IOException{
 		
-		String content = "Hello Java";
+		String content = "Hello Java\r\nGood";//\r是linux系统下的换行符，\n是windows下的换行符
 		
 		FileWriter fWriter = new FileWriter("d:\\java.txt");
 		
@@ -30,5 +32,31 @@ public class FileWriterTest {
 		fWriter.flush();//刷新该流的缓冲区去。
 		
 		fWriter.close();//关闭流，但是要先刷新该流对象。
+	}
+	
+	/**
+	 * IO异常的专业处理。k:\\demo.txt 找不到这个文件
+	 */
+	@Test(expected=java.io.FileNotFoundException.class)
+	public void IOExceptionTest(){
+		
+		FileWriter fWriter = null;
+		try {
+			fWriter = new FileWriter("k:\\demo.txt");
+			fWriter.write("ioexception");
+			fWriter.flush();
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
+		finally{
+			//为了程序的严谨，finally要关闭fWriter对象，所以要先判断它是否为null，不为null才出现close。
+			try {
+				if (fWriter != null) {
+					fWriter.close();
+				}
+			} catch (IOException e2) {
+				System.out.println(e2.toString());
+			}
+		}
 	}
 }
